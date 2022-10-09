a9f5ac89f0bd15e109ba472e8d5d88740b1a80fa
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2022-10-09 22:56:55 +0530

    WSS

---
+++ my-app/constants/index.js
@@ -0,0 +1,4 @@
+export const NFT_CONTRACT_ABI = "abi-of-your-nft-contract";
+export const NFT_CONTRACT_ADDRESS = "0x86b16B458A3b65B6F71232F63b746eA5c5efb25F";
+export const TOKEN_CONTRACT_ABI = "abi-of-your-token-contract";
+export const TOKEN_CONTRACT_ADDRESS = "0xe17F7b1910646253277F76d8286Ae24902498c9c";
