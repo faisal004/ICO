b8576feb697e05f15484f49ee45779b5d81a006a
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2022-10-09 22:33:09 +0530

    Sunday

--- hardhat-tutorial/contracts/Lock.sol
+++ hardhat-tutorial/contracts/Lock.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.4;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+
 
 contract Lock {
     uint public unlockTime;
